411e237885515bd3a0f4d7feb268ce0dcc0ba3bd
Sneha Kashyap <snehakashyap020704@gmail.com>
2026-05-30 14:08:39 +0530

    Update faiss_store.py

--- faiss_store.py
+++ faiss_store.py
@@ -1,15 +1,12 @@
 from langchain_community.vectorstores import FAISS
 
-from embeddings.embedding_model import embedding_model
+from embedding_model import embedding_model
 
 
 def create_vectorstore(chunks):
-
     vector_db = FAISS.from_documents(
         chunks,
         embedding_model
     )
-
     vector_db.save_local("faiss_index")
-
     return vector_db
