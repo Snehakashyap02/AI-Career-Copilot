ef32d27356b667a8d87ea37c115d5657ef98e40e
Sneha Kashyap <snehakashyap020704@gmail.com>
2026-05-30 13:50:00 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
 from langchain_classic.agents import initialize_agent
 from langchain_classic.agents import AgentType
 
-from models.llm import llm
+from llm import llm
 from tools.tools import resource_tool
 
 agent = initialize_agent(
